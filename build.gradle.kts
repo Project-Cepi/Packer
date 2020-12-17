4668ee3d3465d6698f416b868b24ae53bdf67aba
LeoDog896 <LeoDog896@hotmail.com>
2020-12-17 12:46:01 -0500

    Base project

---
+++ build.gradle.kts
@@ -0,0 +1,30 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.4.21"
+}
+
+group = "world.cepi.packer"
+version = "1.0"
+
+repositories {
+    mavenCentral()
+    maven(url = "https://nexus.velocitypowered.com/repository/maven-public/")
+}
+
+dependencies {
+
+    implementation("com.velocitypowered:velocity-api:1.1.1")
+
+    testImplementation(kotlin("test-junit5"))
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.6.0")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.withType<KotlinCompile>() {
+    kotlinOptions.jvmTarget = "11"
+}
