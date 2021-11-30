d3eebb3fb4eb53ad83831dc3461d326701457f33
emortal <nathanpenwill88@gmail.com>
2021-11-30 21:05:08 +0000

    Changes

--- src/main/kotlin/world/cepi/packer/PackerCommand.kt
+++ src/main/kotlin/world/cepi/packer/PackerCommand.kt
@@ -1,30 +1,29 @@
 package world.cepi.packer
 
-import com.velocitypowered.api.proxy.ProxyServer
-import com.velocitypowered.api.command.BrigadierCommand
-
-import com.velocitypowered.api.command.CommandSource
-
 import com.mojang.brigadier.builder.LiteralArgumentBuilder
 import com.mojang.brigadier.context.CommandContext
-
+import com.velocitypowered.api.command.BrigadierCommand
+import com.velocitypowered.api.command.CommandSource
+import com.velocitypowered.api.proxy.ProxyServer
 import net.kyori.adventure.text.Component
+import net.kyori.adventure.text.format.NamedTextColor
 
 
 object PackerCommand {
 
     fun register(server: ProxyServer) {
-        val packerNode = LiteralArgumentBuilder.literal<CommandSource>("hash")
+        val packerNode = LiteralArgumentBuilder.literal<CommandSource>("refresh")
             .executes { context: CommandContext<CommandSource> ->
-                val message: Component = Component.text("Hash: ${ResourcepackLoader.createSha1(ResourcepackLoader.url)}")
-                context.source.sendMessage(message)
+                EventListener.hash = EventListener.refreshSha1()
+
+                context.source.sendMessage(Component.text("Refreshed resource pack hash", NamedTextColor.GREEN))
                 1 // indicates success
             }
 
 
         val command = BrigadierCommand(packerNode)
 
-        val meta = server.commandManager.metaBuilder("packer").build()
+        val meta = server.commandManager.metaBuilder("packerrefresh").build()
 
         server.commandManager.register(meta, command)
     }
